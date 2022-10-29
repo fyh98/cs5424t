2a83d678ca8326febd67c36816364eaada256aa0
albert <albert.kakit.leung@gmail.com>
2022-10-29 14:47:34 +0800

    saveClientReport

--- ycql/src/main/java/com/cs5424t/ycql/Utils/BenchMarkStatOverall.java
+++ ycql/src/main/java/com/cs5424t/ycql/Utils/BenchMarkStatOverall.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -37,7 +39,29 @@ public class BenchMarkStatOverall {
 
     // part-1
     public void saveClientReport(){
-        //todo
+        CsvWriter csv = new CsvWriter(this.filePath + "\\client.csv", ',', StandardCharsets.UTF_8);
+        for(int i = 0; i < this.data.size(); i++){
+            try {
+                List<String> client = new ArrayList<>();
+                client.add(String.valueOf(i));
+                int tranxNum = this.data.get(i).getTranxNum();
+                long duration = this.data.get(i).getDuration();
+                List<Long> durationList = this.data.get(i).getDurationList();
+                client.add(String.valueOf(tranxNum));
+                client.add(String.valueOf(duration));
+                client.add(String.valueOf(tranxNum/duration));
+                client.add(String.valueOf(duration/tranxNum));
+                client.add(String.valueOf(durationList.get((int) Math.floor(tranxNum*0.5))));
+                client.add(String.valueOf(durationList.get((int) Math.floor(tranxNum*0.95))));
+                client.add(String.valueOf(durationList.get((int) Math.floor(tranxNum*0.99))));
+
+                String[] c = client.toArray(new String[client.size()]);
+                csv.writeRecord(c);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        csv.close();
     }
 
     // part-2
